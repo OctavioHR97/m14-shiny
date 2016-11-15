f34efe0c97768fc5b05db1cfadaed97b71dd4e90
Octavio <octavio.hermida.97@gmail.com>
2016-11-15 13:48:35 -0800

    Completed exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,11 +1,13 @@
 # Exercise 1: Loading functions
-
+library(plotly)
+library(dplyr)
 # Set your directory
-setwd('~/Documents/info-201/m14-shiny/exercise-1/')
-df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+setwd('~/Desktop/University/Informatics/Modules/m14-shiny/exercise-1/')
 
 # Load your buildMap.R Script
-
+source('./scripts/buildMap.R')
+data.frame <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
 
 # Use your BuildMap function to draw a map of the data
+BuildMap(data.frame, title="Bubble Map of Cities")
 
